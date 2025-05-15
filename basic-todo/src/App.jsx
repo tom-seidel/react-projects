318984d0ab94421a5b98da03a2059ea9df4b5631
Tom Seidel <hi@tomseidel.com>
2025-05-15 16:57:26 +0200

    feat: add list with elements and output

--- basic-todo/src/App.jsx
+++ basic-todo/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
 
-function App() {
+const items = [
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+];
 
+const itemList = items.map((item) => (
+  <li key={item.id}>{item.name}</li>
+));
+
+export default function App() {
   return (
-    <div>
-      <h1>Basic Todo</h1>
-    </div>
-  )
+    <ul>
+      {itemList}
+    </ul>
+  );
 }
-
-export default App
