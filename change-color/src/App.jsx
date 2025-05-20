99d91d70f9c48e0dd68f97a55bf41f58364b62b8
Tom Seidel <hi@tomseidel.com>
2025-05-20 22:26:05 +0200

    feat: add useState, useEffect and buttons

--- change-color/src/App.jsx
+++ change-color/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 
 export default function App() {
+  const [color, setColor] = useState('red');
+
+  useEffect(() => {
+    document.body.style.backgroundColor = color;
+  }, [color]);
 
   return (
     <>
-      <h1>Change Color</h1>
-      <p>It works!</p>
+      <h1>Change Background Color</h1>
+      <div className="controls">
+        <button onClick={() => setColor('red')}>Red</button>
+        <button onClick={() => setColor('green')}>Green</button>
+        <button onClick={() => setColor('blue')}>Blue</button>
+      </div>
     </>
   )
 }
